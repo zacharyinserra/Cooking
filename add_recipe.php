2a3b45165dcf2723efe67eee4976d7a29fe3b0c9
zacharyinserra <38013398+zacharyinserra@users.noreply.github.com>
2019-05-15 13:15:09 -0400

    addslashes

--- add_recipe.php
+++ add_recipe.php
@@ -5,10 +5,14 @@ $new_recipe_name = $_GET["new_recipe_name"];
 $new_meal_type = $_GET["new_meal_type"];
 $new_completion_time = $_GET["new_completion_time"];
 
+$new_recipe_name = addslashes($new_recipe_name);
+$new_meal_type = addslashes($new_meal_type);
+$new_completion_time = addslashes($new_completion_time);
+
 echo "<h2>Trying to add new recipe: $new_recipe_name, $new_meal_type, $new_completion_time</h2>";
 $sql = "INSERT INTO recipes (recipeID, recipe_name, meal_type, completion_time)
 		VALUES (NULL, '$new_recipe_name', '$new_meal_type', '$new_completion_time')";
-$result = $mysqli->query($sql);
+$result = $mysqli->query($sql) or die(mysqli_error($mysqli));
 
 include "search_all_recipes.php";
 
